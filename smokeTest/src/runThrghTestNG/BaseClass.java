5da9cd8c7555d2dfbe833656b089a6e115bb6560
Somesh Bansal <somesh.bansal@techizen.in>
2012-09-18 11:53:10 +0530

    Browser Chrome Simulation based on varoius OS

--- smokeTest/src/runThrghTestNG/BaseClass.java
+++ smokeTest/src/runThrghTestNG/BaseClass.java
@@ -22,20 +22,37 @@ public class BaseClass {
 
     //The annotated method will be run before any test method belonging to the classes inside the <test> tag is run
     @BeforeTest
-    @Parameters({"program", "drvr"})
-    public void setUp(String program, String drvr) throws Exception {
-        
+    @Parameters({"program", "drvr", "os"})
+    public void setUp(String program, String os, String drvr) throws Exception {
+
         av = new AccountValues(program);
         File directory = new File(".");
         switch (drvr) {
             case "Chrome":
-                System.setProperty("webdriver.chrome.driver", directory.getCanonicalPath() + File.separator + "chromedriver.exe");
+                os:
+                switch (os) {
+                    case "Linux32":
+                        System.setProperty("webdriver.chrome.driver", directory.getCanonicalPath() + File.separator + "lib" + File.separator + "chromedriver_linux32"
+                                + File.separator + "chromedriver.exe");
+                        break os;
+                    case "Linux64":
+                        System.setProperty("webdriver.chrome.driver", directory.getCanonicalPath() + File.separator + "lib" + File.separator + "chromedriver_linux64"
+                                + File.separator + "chromedriver.exe");
+                        break os;
+                    case "Mac":
+                        System.setProperty("webdriver.chrome.driver", directory.getCanonicalPath() + File.separator + "lib" + File.separator + "chromedriver_mac"
+                                + File.separator + "chromedriver.exe");
+                        break os;
+                    default:
+                        System.setProperty("webdriver.chrome.driver", directory.getCanonicalPath() + File.separator + "lib" + File.separator + "chromedriver_win"
+                                + File.separator + "chromedriver.exe");
+                }
                 driver = new ChromeDriver();
                 break;
             default:
                 driver = new FirefoxDriver();
         }
-        driver.manage().window().maximize();   
+        driver.manage().window().maximize();
         driver.get(av.getTokenValue("programURL"));
         ip.isTitlePresent(driver, av.getTokenValue("loginPageTitle"));
     }
